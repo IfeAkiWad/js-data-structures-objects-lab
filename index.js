b06d2f5b7017d0f0a59ae400b9fa20b0365169e5
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-02-21 19:28:12 -0600

    commit tree c0b65efca64f448e0546b68a452ec7e3d136379d

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const driver = {} //object
+
+
+// This function should not mutate the driver 
+// and should return a new driver that 
+// has an updated value for the key passed in.
+function updateDriverWithKeyAndValue(driver, key, value) {
+    // newDriver = {}
+    return Object.assign({}, driver, { [key]: value });
+}
+
+function  destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    // newDriver = {}
+    return Object.assign(driver, { [key]: value });
+}
+
+function deleteFromDriverByKey(driver, key) {
+    let newobj = Object.assign({}, driver, key);
+    delete newobj[key]
+    return newobj
+}
+
+function destructivelyDeleteFromDriverByKey(object, key) {
+    delete object[key]
+    return object
+}
